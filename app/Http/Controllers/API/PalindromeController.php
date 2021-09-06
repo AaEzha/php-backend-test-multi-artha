fbeffa53d483bafc215534a9d12eaf178a708efb
AaEzha <aaezha@gmail.com>
2021-09-06 18:41:18 +0700

    Palindrome Check

---
+++ app/Http/Controllers/API/PalindromeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PalindromeResource;
+use Illuminate\Http\Request;
+
+class PalindromeController extends Controller
+{
+    public function __invoke($string = null)
+    {
+        $status = false;
+
+        $check = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+
+        if (strrev($check) == $check && strlen($check) >= 1) {
+            $status = true;
+        }
+
+        $data['text'] = $string;
+        $data['status'] = $status;
+
+        return new PalindromeResource($data);
+    }
+}
